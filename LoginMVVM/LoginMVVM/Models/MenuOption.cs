edbc8a177ed0d1ee6b9626bba53c1d30898d3c7e
Alexis Novas <47201459+alexisnovas@users.noreply.github.com>
2021-08-28 01:06:31 -0400

    Update MenuOption.cs

--- LoginMVVM/LoginMVVM/Models/MenuOption.cs
+++ LoginMVVM/LoginMVVM/Models/MenuOption.cs
@@ -6,6 +6,7 @@ namespace LoginMVVM.Models
 {
     public class MenuOption
     {
+        //Menu Option props
         public string Title { get; set; }
         public string Image { get; set; }
     }
